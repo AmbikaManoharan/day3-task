d03a4c68cdbc5d81606989d4510bcafaec4861e6
AmbikaManoharan <112929714+AmbikaManoharan@users.noreply.github.com>
2022-09-30 15:45:54 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,44 @@
+
+    let myResume={
+        "basics": {
+          "name": "Ambika. M",
+          "email": "manoharan.ambika@gamil.com",
+          "phone": 9362019273,
+          "degree": "Msc. Software",
+        
+          "location": {
+            "address": "Supertech Icon",
+            "postalCode": 201014,
+            "city": "Ghaziabad",
+            "state": "Uttar Pradesh",
+            "country": "India"
+          },
+          "profiles": [
+            {
+              
+              "github":"https://github.com/AmbikaManoharan"
+            }
+          ],
+                  
+        
+        "education": [
+          {
+            "institution": "Sengunthar Arts and Science College",
+            "department": "Software",
+            "studyType": "fulltime",
+            "batch start year": 2000,
+            "batch end year": 2005
+            
+          }
+        ],
+        
+        "languages": [
+          {
+            "language": "Tamil, English, Hindi",
+          }
+        ],
+      }
+    }
+        console.log(myResume)
+      
+  
